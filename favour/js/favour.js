f673cdc50e36e3268cbd634b9a6096d989b30b70
molandi <18268832486@163.com>
2017-06-10 18:23:08 +0800

    you can upload

---
+++ favour/js/favour.js
@@ -0,0 +1,56 @@
+					//绑定删除按钮的触发事件 
+				$(".delete").click(function(){ 
+				
+				$(".mask").css("opacity","0.3").show(); 
+				showDialog(); 
+				$(".dialog").show(); 
+				}); 
+				
+				/* 
+				* 根据当前页面于滚动条的位置，设置提示对话框的TOP和left 
+				*/ 
+				function showDialog(){ 
+				var objw=$(window);//当前窗口 
+				var objc=$(".dialog");//当前对话框 
+				var brsw=objw.width(); 
+				var brsh=objw.height(); 
+				var sclL=objw.scrollLeft(); 
+				var sclT=objw.scrollTop(); 
+				var curw=objc.width(); 
+				var curh=objc.height(); 
+				//计算对话框居中时的左边距 
+				var left=sclL+(brsw -curw)/2; 
+				var top=sclT+(brsh-curh)/2; 
+				
+				//设置对话框居中 
+				objc.css({"left":left,"top":top}); 
+				
+				} 
+				
+				//当页面窗口大小改变时触发的事件 
+				$(window).resize(function(){ 
+				
+				if(!$(".dialog").is(":visible")){ 
+				return; 
+				} 
+				showDialog(); 
+				}); 
+				
+				
+				//取消按钮事件 
+				$("#noOk").click(function(){ 
+				$(".dialog").hide(); 
+				$(".mask").hide(); 
+				}); 
+				
+				//确定按钮事假 
+				$("#ok").click(function(){ 
+				
+				$(".dialog").hide(); 
+				$(".mask").hide();
+				$("#content").remove();
+				console.log($("#content").length);
+					if($("#content").length===0){
+					location.href="./no-address.html";
+				}
+				}); 
